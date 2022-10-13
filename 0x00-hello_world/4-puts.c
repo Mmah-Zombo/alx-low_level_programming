df8a53168b550c074794b86ca5285cceb663bc7a
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-13 15:06:49 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/*** I do not have any logical thing tosay as a comment
+/**
+ * I do not have any logical thing tosay as a comment
+ * main - main program
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts( "\"Programming is like building a multilingual puzzle");
 	return(0);
 }
