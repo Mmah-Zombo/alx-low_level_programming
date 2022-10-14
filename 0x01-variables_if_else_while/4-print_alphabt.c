293a67f6459a06efd83c5f63f827fa7c22168b3c
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-14 15:43:47 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 	int letter;
 
 	for (letter = 97; letter < 123; letter++)
-		if (letter != 101 || letter != 113)
+		if (letter != 101 && letter != 113)
 			putchar(letter);
 	putchar('\n');
 
