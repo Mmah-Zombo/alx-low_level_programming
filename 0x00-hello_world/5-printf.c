1433659c54796dd53360c4e7b4f9c278a5f0ac00
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-13 18:12:40 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - start here
+ * Return: 0 at the end of code
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
