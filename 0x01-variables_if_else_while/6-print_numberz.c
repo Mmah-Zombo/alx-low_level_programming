b54b89b673e5de7506191d1d741bd834cfb0f0fe
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-14 16:13:27 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 30; num < 40; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+
+}
