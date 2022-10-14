77a494f62e0a3c9023e885142dfccee62717f804
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-13 18:37:48 -0700

    tsk8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - start here
+ * Return: 1 at the end
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
