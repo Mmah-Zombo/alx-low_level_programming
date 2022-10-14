41db303b7af014ab140a0b59b29f74210fc38c62
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-14 15:02:32 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - enter here
+ * Return: 0 always
+ */
+int main(void)
+
+{
+
+	int check;
+
+	for (check = 97; check <123; check++)
+		putchar(check);
+
+	putchar('\n');
+
+	return (0);
+
+}
