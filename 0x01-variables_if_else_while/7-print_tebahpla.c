28fa5ec9f7589cf148e33cc2e723bb90ad216765
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-14 18:13:53 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - enter here
+ * Return: 0 always
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 122; letter > 96; letter--);
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+
+
+
+}
