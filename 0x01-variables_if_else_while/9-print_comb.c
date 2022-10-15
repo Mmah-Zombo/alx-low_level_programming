bb3304070387f93cb07167ba131f5af0ac928111
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-14 18:49:54 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,9 @@ int main(void)
 	int comb;
 
 	for (comb = 48; comb < 58; comb++)
-		putchar(comb);
-		putchar(',');
+		putchar(comb)
+		if (comb < 57)
+			putchar(',');
 	putchar('\n');
 
 	return (0);
