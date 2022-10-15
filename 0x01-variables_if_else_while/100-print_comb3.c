8b28c997d3678af7945c25f89e5adc1fdd59310e
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-14 20:30:17 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,19 @@
 int main(void)
 {
 	int num, num2, filter;
-	filter = 1;
-	num2 = 1;
 
-	for (num = 0; num < 10; num++)
+	filter = 1;
+	num2 = 49;
+
+	for (num = 48; num < 57; num++)
 	{
-		for (; num2 <10; num2++)
+		for (; num2 <= 57; num2++)
 		{
 			putchar(num);
 			putchar(num2);
 			num2 = filter + 1;
 			filter += 1;
-			if (num + num2 != 17)
+			if (num + num2 != 105)
 			{
 				putchar(',');
 				putchar(' ');
