f9cc3c09792674022847f5784be59509f7abdcf5
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-18 14:06:20 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-
-void print_alphabet(void);
 /**
  * main - entry
  * Return: 0 always
@@ -10,15 +8,5 @@ void print_alphabet(void);
 int main(void)
 {
 	print_alphabet();
-	return (0);
-}
-
-/**
- * print_alphabet - writes allphabet from a to z
- * Return: 0 always
- */
-void print_alphabet(void)
-{
-	printf("abcdefghijklmnopqrstuvwxyz\n");
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+void _putchar(void)
+{
+	printf("abcdefghijklmnopwrstuvwxyz\n");
+}
+
+
+void print_alphabet(void)
+{
+	_putchar();
+}
