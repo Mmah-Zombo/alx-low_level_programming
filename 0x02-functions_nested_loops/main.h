909593ac91ccd6e59788096f2ab2ca726f861bbe
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-19 04:52:03 -0700

    task1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
+int _putchar(char c);
 void print_alphabet(void);
-
-void print_alphabet(void)
-{
-	int ltr;
-
-	for (ltr = 97; ltr < 123; ltr++)
-	{
-		putchar(ltr);
-	}
-	putchar(10);
-}
 #endif
