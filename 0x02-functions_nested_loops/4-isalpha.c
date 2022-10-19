af25d2b0cfda2c2b16206e81459b59f2cd0960f4
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-19 06:13:32 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - start here
+ * @c: is the letter to be checked
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
