98e5574a993cdbed89a3808e202ff44259a1b047
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-20 10:03:10 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - enter her
+ * @c: value to be checked
+ *
+ * Return: 1 or 0 
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
