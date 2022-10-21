4f4236d3f084dca6d4ed3fa988716521fe3eb9e8
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-21 09:53:06 -0700

    task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - start here
+ * @size: trinagle size
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+	b = 1;
+
+	for (a = 1; a < size + 1; a++)
+	{
+		c = a;
+
+		while (c < size)
+		{
+			_putchar(' ');
+			c++;
+		}
+
+		c = 1;
+
+		while (c <= b)
+		{
+			_putchar('#');
+			c++;
+		}
+		b++;
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}
