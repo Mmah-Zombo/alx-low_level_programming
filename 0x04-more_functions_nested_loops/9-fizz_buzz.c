c8ea710e3d525d1c867665e45150db067e8d3e4e
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-20 18:36:25 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - enter here
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		if (num % 5 == 0)
+		{
+			if (num == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
+		if (num % 5 == 0 && num % 3 == 0)
+		{
+			printf("FizzBuzz ";
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
