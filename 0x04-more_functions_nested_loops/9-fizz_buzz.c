ad18e6ceca898717f19108b3268fe96e7a794c66
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-20 18:53:56 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
+		if (num % 3 == 0 && num % 5 != 0)
 		{
 			printf("Fizz ");
 		}
-		if (num % 5 == 0)
+		if (num % 5 == 0 && num % 3 != 0)
 		{
 			if (num == 100)
 			{
