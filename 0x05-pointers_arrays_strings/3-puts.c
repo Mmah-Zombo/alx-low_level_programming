de4f06ad370760989912c85ffff15c2dd2f9a4e2
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-24 09:36:04 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - start here
+ * @str: sentences to look at
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 void reset_to_98(int *n);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
+void _puts(char *str);
 #endif
