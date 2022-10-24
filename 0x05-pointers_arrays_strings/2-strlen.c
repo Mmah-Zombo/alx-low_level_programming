991a978c03b15b64dc1ecfc5e95c584a95f1bf15
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-24 07:09:39 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - start here
+ * @s: pointer
+ */
+int _strlen(char *s)
+{
+	int a = strlen(s);
+	return (a);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 
 void reset_to_98(int *n);
+int _strlen(char *s);
 #endif
