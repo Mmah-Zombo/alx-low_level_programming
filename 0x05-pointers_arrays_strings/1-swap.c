642812ba37e7a9c05b182461fe318b9b66908838
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-24 07:21:21 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - start here
+ * @a: value one 
+ * @b: value two
+ */
+void swap_int(int *a, int *b)
+{
+	int no = *a;
+
+	*a = *b;
+	*b = no;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 
 void reset_to_98(int *n);
 int _strlen(char *s);
+void swap_int(int *a, int *b);
 #endif
