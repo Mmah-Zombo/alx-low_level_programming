769ec5b295308bba4e167be65166e3ac54c7354c
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-26 00:46:27 +0100

    task11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,42 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
 
 /**
- * _atoi - start here
+ * _atoi - random password generator for 101-crackme
  * @s: value to be tested
  *
- * Return: converted string
+ * Return: always 0
  */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int i, j, k, je;
+
+	srand(time(NULL));
+	while (je != 2772)
+	{
+		i = k = je = 0;
+		while ((2772 - 122) > je)
+		{
+			j = rand() % 62;
+			p[i] = s[j];
+			je += s[j];
+			i++;
+		}
+		while (s[k])
+		{
+			if (s[k] == (2772 - je))
+			{
+				p[i] = s[k];
+				je += s[k];
+				i++;
+				break;
+			}
+			k++;
+		}
+	}
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
 }
