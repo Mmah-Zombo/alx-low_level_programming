c91c9b9f4a2fb84e80995f095d4ef72e898a7625
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-25 15:43:58 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcpy - start here
+ * @dest: character one
+ * @src: character two
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	memset(dest, '\0', sizeof(dest));
+	strcpy(dest, src);
+}
