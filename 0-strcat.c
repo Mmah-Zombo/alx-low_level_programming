12df9c5d41c8257758397bbee337904a5a800ef1
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-26 16:10:06 +0100

    task0

---
+++ 0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - start here
+ * @dest: value one
+ * @src: value two
+ *
+ * Return: a pointer to the result string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *dest, char *src);
+
+#endif
