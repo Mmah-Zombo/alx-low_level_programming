06b6337d501c152375fb0997be4a7e21c59ad403
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-26 17:10:43 +0100

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - the name of the function
+ * @s1: value one
+ * @s2: value two
+ *
+ * Return: 0 or a positive number or a negative number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	 return (strcmp(s1, s2));
+}
