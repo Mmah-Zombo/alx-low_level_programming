fe4203fb09e156fd28634a69008482cac8cb0f14
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-26 01:03:30 +0100

    task10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - start here
+ * @s: value to convert
+ *
+ * Return: an int 
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
