a738273a812cf3b100dccc63701e1d8e3f325238
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-27 19:59:20 +0100

    task9

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_buffer - here start
+ * @b: array
+ * @size: size of content
+ */
+void print_buffer(char *b, int size)
+{
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+void print_number(int n);
 #endif
