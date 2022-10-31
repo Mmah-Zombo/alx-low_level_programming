9656c4da998cb1cc920d5f4794f0720d8f63f7f7
M'mah Zombo <www.mariamzombo@gmail.com>
2022-10-31 15:12:21 +0000

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_memset - start here
+ * @s: i don't know yet
+ * @b: idk
+ * @n: idk
+ *
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H
+#define MAIN_H
+char *_memset(char *s, char b, unsigned int n);
+#endif
