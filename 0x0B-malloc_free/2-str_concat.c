a42765e6108e6428906715b0f921b161590fbd5c
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-09 02:25:27 +0000

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,18 @@ char *str_concat(char *s1, char *s2)
 {
 	char *anoda;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	anoda = strcat(s1, s2);
 
-	unsigned int i = malloc(sizeof(anoda));
+	char *i = malloc(sizeof(anoda));
 
 	if (i == NULL)
 	{
