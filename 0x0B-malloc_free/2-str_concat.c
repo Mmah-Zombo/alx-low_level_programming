557a6ab57fbfa3031b3d58b8de5c803733544b20
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-09 02:53:59 +0000

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,12 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	strout = strcat(s1, s2);
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
 	return (strout);
 }
