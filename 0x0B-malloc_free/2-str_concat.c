f05c0d2ba316437fd404014ab21dbf3917dcf41a
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-09 02:16:01 +0000

    task2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - function name
+ * @s1: value one
+ * @s2: string two
+ *
+ * Return: a pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *anoda;
+
+	anoda = strcat(s1, s2);
+
+	if (anode == NULL)
+	{
+		return (NULL);
+	}
+
+	if (sizeof(anoda) == NULL)
+	{
+		return (NULL);
+	}
+	return (anoda);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,4 +3,5 @@
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
