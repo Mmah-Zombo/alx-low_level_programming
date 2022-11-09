1d23dc41bf6382fb2b91156baf6e27e5165a1f13
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-09 01:55:53 +0000

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - function name
+ * @str: string to be duplicated
+ *
+ * Return: a pointer
+ */
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
