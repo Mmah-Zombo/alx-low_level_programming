33aafbf7c4fa379c79ecb506533219b8cb2fd053
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-10 23:17:19 +0000

    fixed requirement error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- * malloc_cheched - function name
+ * malloc_checked - function name
  * @b: variable
  *
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-	if (malloc(b) == NULL) 
+	if (malloc(b) == NULL)
 	{
 		exit(98);
 	}
