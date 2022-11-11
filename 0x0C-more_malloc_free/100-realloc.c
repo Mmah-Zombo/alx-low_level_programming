045dbf72d5ca8a9f7a3bbd3461e81f958f39d698
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-11 02:54:26 +0000

    task5 sketch

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - fuction name
+ * @ptr: value one
+ * @old_size: value two
+ * @new_size: value three
+ *
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+}
