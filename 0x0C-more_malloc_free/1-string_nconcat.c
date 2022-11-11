6795ddee633c4f1562627c0f4541bf66d51dbeb2
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-11 00:53:28 +0000

    added NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p = malloc((size1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
-		return (0);
+		return (NULL);
 
 	for (i = 0; i < size1; i++)
 	{
