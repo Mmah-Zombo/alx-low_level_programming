54e826d5960e808343923b9c60b3c75f47aa7314
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-11 00:43:02 +0000

    fixed all errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,44 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat - function name
- * @s1: one
- * @s2: two
- * @n: another var
- *
- * Return: a pointer
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *dest;
-	int i;
+	char *p;
+	unsigned int size1, size2, i;
 
-	dest = malloc((strlen(s1) + n + 1) * sizeof(char));
-
-	if (dest == NULL)
-	{
-		return (NULL);
-	}
-
-	if (n >= strlen(s2))
-	{
-		n = strlen(s2);
-	}
+	size1 = strlen(s1);
+	size2 = strlen(s2);
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	
-	dest = s1;
 
-	for (i = 0 ; i < n && s2[i] != '\0' ; i++)
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
 	{
-		dest[strlen(dest) + i] = s2[i];
+		p[i] = s1[i];
 	}
-	dest[strlen(dest) + i] = '\0';
 
-	return (dest);
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
