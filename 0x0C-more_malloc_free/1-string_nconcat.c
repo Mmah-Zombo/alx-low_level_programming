0732416422fcd706c25cd8f55d66282a27f4e6c4
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-11 00:44:22 +0000

    fixed all errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,16 +11,23 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int size1, size2, i;
-
-	size1 = strlen(s1);
-	size2 = strlen(s2);
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
 
 	if (n > size2)
 	n = size2;
