b31616b576e8361ea94824134b7a2c33839be9e2
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-14 19:33:06 +0000

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-
-	char * name;
+/**
+ * struct dog - data structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog with name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
 	float age;
-	char * owner;
-}
+	char *owner;
+};
+
+typedef struct dog dog_t;
 
 #endif
