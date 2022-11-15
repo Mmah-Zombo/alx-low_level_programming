f496d885f09844269e3ff45f847751d2da892319
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-15 02:48:50 +0000

    task4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * new_dog - function
+ * @name: v1
+ * @age: v2
+ * @owner: v3
+ *
+ * Return: dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *name2, *owner2;
+
+	struct dog;
+
+	name2 = name;
+	owner2 = owner;
+	
+	if (0)
+	{
+		return (NULL);
+	}
+
+}
