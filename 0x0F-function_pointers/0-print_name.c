c2256dce12edc1d6c571afa98aab31a753cfb283
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-16 23:48:00 +0000

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - name of function
+ * @name: pointer
+ * @f: funcrion pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
