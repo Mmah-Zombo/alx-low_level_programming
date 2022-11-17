6f2d8e42b04d398e46ae3455b05b722e28f7af81
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-17 00:44:43 +0000

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,11 +18,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			return (-1);
 		}
-		for (a = 0; a < size; i++)
+		for (a = 0; a < size; a++)
 		{
 			if (cmp(array[a]))
 			{
-				return (u);
+				return (a);
 			}
 		}
 	}
