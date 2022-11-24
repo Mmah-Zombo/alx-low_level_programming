21db03b18b6ae40d315dd95f4cf880eb5179bcd4
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-24 23:38:22 +0000

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,22 +3,6 @@
 #include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
-
-
-/**
  * print_list - function that prints d nmbr of elements a list
  * @h: a list_h pionter
  *
--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,4 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LIST_H
+#define LIST_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: Singly linked lists
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 #endif
