07d8426c9ee23790173f44ff437d7b5b38b38818
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-25 02:53:16 +0000

    task2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_node - fnctn that adds new node @ d beginning of a list
+ * @head: pointer to a pointer
+ * @str: pointer to a string
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	const char *str2;
+	str2 = strdup(str);
+
+
+}
