9fa51c6a8810c91e92fdd24d345f213c9008e5ee
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-25 02:28:11 +0000

    task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-void before_main() __attribute__((constructor));
-
+void before_main(void) __attribute__((constructor));
 
 /**
  * before_main - function that executes b4 main
  */
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 		"I bore my house upon my back!\n");
