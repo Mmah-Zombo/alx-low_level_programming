77b1d959f6ffd5addb8ee46d3ba032121743056e
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-29 22:01:53 +0000

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *ptr = *head;
 	listint_t *ptr2 = *head;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		return (-1);
 	}
