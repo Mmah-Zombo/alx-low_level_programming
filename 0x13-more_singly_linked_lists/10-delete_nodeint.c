df30083cecdba0ff3b1ec253b28f72fbfcfe2d8e
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-29 22:41:50 +0000

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,7 +3,12 @@
 #include "lists.h"
 
 /**
+ * delete_nodeint_at_index - deletes the node at index 'index'
+ *                           of a listint_t linked list.
+ * @head: pointer to the head of the list
+ * @index: position of the node, starting at 0
  *
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
@@ -18,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(ptr)
+		free(ptr);
 		return (1);
 	}
 
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: pointer to the list's head
+ * @idx: the index of the list where the new node should be added.
+ * @n:
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+
+}
