2e055dfcd1af4988e816a0aded6855e3dcaf701d
M'mah Zombo <www.mariamzombo@gmail.com>
2022-11-29 22:34:11 +0000

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,9 +9,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int counter = 0;
 	listint_t *ptr = *head;
-	listint_t *ptr2 = *head;
+	listint_t *ptr2;
 
-	if (*head == NULL)
+	if (ptr == NULL)
 	{
 		return (-1);
 	}
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(ptr)
-			return (1);
+		return (1);
 	}
 
 	while (counter < (index - 1))
@@ -36,17 +36,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		counter++;
 	}
 
-	if (ptr->next != NULL)
-	{
-		ptr2->next = ptr->next;
-		free (ptr);
-		return (1);
-	}
 
-	if (ptr->next == NULL)
-	{
-		ptr2->next == NULL;
-		free (ptr);
-		return (1);
-	}
+	ptr2->next = ptr->next;
+	free (ptr);
+	return (1);
+
 }
