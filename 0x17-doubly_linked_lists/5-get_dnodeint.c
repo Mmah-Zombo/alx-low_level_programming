05bdf88c597a0b27f2705891aa5b916a8b563c9d
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-14 01:13:30 +0000

    task5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
 #include "lists.h"
 
-int sum_dlistint(dlistint_t *head)
+/**
+ * get_dnodeint_at_index - Locates a node in a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ * @index: The node to locate.
+ *
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the address of the located node.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	if 
+    unsigned int i;
+    dlistint_t *current;
+
+    if (head == NULL)
+        return (NULL);
+
+    current = head;
+    for (i = 0; i < index; i++)
+    {
+        if (current->next == NULL)
+            return (NULL);
+        current = current->next;
+    }
+    return (current);
 }
