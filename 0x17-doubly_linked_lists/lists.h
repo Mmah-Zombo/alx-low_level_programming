516cd01a7344aaf816c1dc14244c0b1af8cd8ba5
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-14 00:19:12 +0000

    tasks 0 - 3

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,4 +29,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-#endif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
+#endif
