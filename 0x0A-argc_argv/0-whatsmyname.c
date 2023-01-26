f6587855888aa4c1d26abbb738d6bb1fc31f75d9
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-26 03:27:02 +0000

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print the name of the executable.
- * @argc: argument count or size of arguments.
- * @argv: argument vector
+ * main - the main function
+ * @argc: number of arguments passed to main
+ * @argv: the list of arguments passed
  *
- * Return: always 0.
+ * Return: 0
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", *argv);
 	return (0);
-}
+} 
