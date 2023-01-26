abad25fba11dbc33e7f00ee3b7dee042409cb15f
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-26 03:21:25 +0000

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  *
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char **argv)
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	
 	return (0);
-}
+} 
