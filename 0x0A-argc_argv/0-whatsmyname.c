adb253b0c3b2cee93475dfb2e3877c679e765544
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-26 03:23:50 +0000

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - the main function
  * @argc: number of arguments passed to main
@@ -10,6 +10,5 @@
 int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", *argv);
-	
 	return (0);
 } 
